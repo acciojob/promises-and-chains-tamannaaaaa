4282393939d145ee4ec951d3add8435459a7fb4c
Tamanna Singh <129686086+tamannaaaaa@users.noreply.github.com>
2025-01-25 21:44:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 //your JS code here. If required.
+document.getElementById("userForm").addEventListener("submit", function(event){
+	event.preventDefault();
+
+const ageIn = document.getElementById("age").value;
+const nameIn = document.getElementById("name").value;
+
+if(!ageIn || !nameIn){
+	alert("These are mandatory fields!!");
+	return;
+}
+
+const age = parseInt(ageIn, 10);
+const name = nameIn;
+
+const promise = new Promise((resolve, reject) =>{
+	setTimeout(() =>{
+		if(age >= 18){
+			resolve("Welcome, ${name}. You can vote.");
+		}
+		else{
+			reject("Oh sorry ${name}. You aren't old enough");
+		}
+	}, 4000);
+});
+
+promise
+	.then((message) => {
+		alert(message);
+	})
+	.catch((error) =>{
+		alert(error);
+	})
+})
